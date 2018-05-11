cd94043c8b8ab0fe8d57dca2145baf4fc4e03f94
Fifi1996 <zhuangfifi@gmail.com>
2018-05-11 22:26:18 +0800

    搭建前台页面

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
-from flask import Flask
+#coding:utf8
+from flask import Flask,render_template
 
 app = Flask(__name__)
 app.debug = True
@@ -7,3 +8,7 @@ from app.admin import admin as admin_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(admin_blueprint,url_prefix="/admin")
+#404
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("home/404.html"),404
--- app/home/views.py
+++ app/home/views.py
@@ -1,6 +1,56 @@
 from . import home
+from flask import render_template,redirect,url_for
+import os
 
-
+#登陸
+@home.route("/login/")
+def login():
+    return render_template("home/login.html")
+#退出
+@home.route("/logout/")
+def logout():
+    return redirect(url_for("home.login"))
+#注冊
+@home.route("/regist/")
+def regist():
+    return render_template("home/regist.html")
+#會員中心
+@home.route("/user/")
+def user():
+    return render_template("home/user.html")
+#密碼
+@home.route("/pwd/")
+def pwd():
+    return render_template("home/pwd.html")
+#評論記録
+@home.route("/comments/")
+def comments():
+    return render_template("home/comments.html")
+#登録日誌
+@home.route("/loginlog/")
+def loginlog():
+    return render_template("home/loginlog.html")
+#收藏
+@home.route("/moviecol/")
+def moviecol():
+    return render_template("home/moviecol.html")
+#列表
 @home.route("/")
 def index():
-    return "<h1 style='color:green'>this is home</h1>"
+    return render_template("home/index.html")
+#動畫
+@home.route("/animation/")
+def animation():
+    return render_template("home/animation.html")
+#搜索
+@home.route("/search/")
+def search():
+    return render_template("home/search.html")
+#詳情
+@home.route("/play/")
+def play():
+    return render_template("home/play.html")
+
+
+
+
